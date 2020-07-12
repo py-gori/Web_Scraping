9c26341f80e294894ba53336080659ee441254f0
py-gori <52909397+py-gori@users.noreply.github.com>
2020-07-12 10:07:34 +0900

    20200712_modified

--- Settings/settings.py
+++ Settings/settings.py
@@ -8,5 +8,5 @@ SITE_URL = 'https://mag.osdn.jp/'
 
 # LINE settings
 LINE_URL = 'https://notify-api.line.me/api/notify'
-TOKEN = 'VnhsnsBe22utxienQip46AjFBTT2r3UW0g0PbL5FcQF'
+TOKEN = ''
 PICTURE_DIR = 'C://Users//yk28j\py-gori//Web_Scraping//Picture//' + TODAY
